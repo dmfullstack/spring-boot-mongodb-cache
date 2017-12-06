fdd46f354948aaebdb5a3143f417854ed6c7bacf
jeonguk2 <jeonguk.lee@iriver.com>
2017-12-06 18:49:29 +0900

    Redis Configuration Edit

--- src/main/java/com/jeonguk/config/RedisConfiguration.java
+++ src/main/java/com/jeonguk/config/RedisConfiguration.java
@@ -5,11 +5,39 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @EnableCaching
 @Configuration
 public class RedisConfiguration extends CachingConfigurerSupport {
 
+  @Bean
+  public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+    RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(factory);
+   
+    Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+    ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+    // 값의 직렬화 Jackson2JsonRedisSerializer。
+    redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+    redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+    // 키의 직렬화 StringRedisSerializer。
+    redisTemplate.setKeySerializer(jackson2JsonRedisSerializer);
+    redisTemplate.setHashKeySerializer(jackson2JsonRedisSerializer);
+    redisTemplate.afterPropertiesSet();    
+    return redisTemplate;
+  }
+  
   @Bean
   @Override
   public CacheErrorHandler errorHandler() {
